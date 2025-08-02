fe189ef51d592d32d3e3d73a46d9824810ee44db
Eden Sheiko <edensheiko@gmail.com>
2025-08-02 23:47:37 +0300

    added comments

--- SW/Inc/System.h
+++ SW/Inc/System.h
@@ -1,8 +1,8 @@
-/*
- * System.h
- *
- *  Created on: Jul 30, 2025
- *      Author: eden
+/**
+ * @file    System.h
+ * @brief   System module header file
+ * @date    Jul 30, 2025
+ * @author  eden
  */
 
 #ifndef INC_SYSTEM_H_
@@ -12,42 +12,47 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdbool.h>
+
 #include "main.h"
 
-#define ADC_SIZE 1
-#define START_MSG_SIZE 5
-#define STOP_MSG_SIZE 4
-#define DEBOUNCE_TIME 20
+/* Public macros */
+#define ADC_SIZE            1
+#define START_MSG_SIZE      5
+#define STOP_MSG_SIZE       4
+#define DEBOUNCE_TIME       20
 
-extern TIM_HandleTypeDef htim10;
-extern ADC_HandleTypeDef hadc1;
-extern TIM_HandleTypeDef htim11;
-extern UART_HandleTypeDef huart2;
-extern uint8_t rx_buffer[RX_BUFF_SIZE];
+/* External handles */
+extern TIM_HandleTypeDef     htim10;
+extern TIM_HandleTypeDef     htim11;
+extern ADC_HandleTypeDef     hadc1;
+extern UART_HandleTypeDef    huart2;
 
+extern uint8_t               rx_buffer[RX_BUFF_SIZE];
+
+/* Error codes for system module */
 typedef enum {
-	SYSTEM_OK = 0,
-	SYSTEM_ERROR_TIM10,
-	SYSTEM_ERROR_TIM11,
-	SYSTEM_ERROR_ADC
+    SYSTEM_OK = 0,
+    SYSTEM_ERROR_TIM10,
+    SYSTEM_ERROR_TIM11,
+    SYSTEM_ERROR_ADC
 } system_module_error_t;
 
+/* System state machine states */
 typedef enum {
     SYS_STATE_IDLE = 0,
     SYS_STATE_RUNNING,
     SYS_STATE_ERROR
 } system_states_t;
 
+/* Module configuration structure */
 typedef struct module_config_s {
     uint8_t start_msg[START_MSG_SIZE];
     uint8_t stop_msg[STOP_MSG_SIZE];
 } module_config_t;
 
-
+/* Public function declarations */
 void main_system_loop(void);
-
 system_module_error_t adc_start_mesure(void);
 system_module_error_t adc_stop_mesure(void);
-
 
 #endif /* INC_SYSTEM_H_ */
--- SW/Src/System.c
+++ SW/Src/System.c
@@ -1,129 +1,155 @@
-  /*
- * System.c
- *
- *  Created on: Jul 30, 2025
- *      Author: eden
+/**
+ * @file    System.c
+ * @brief   System control logic implementation
+ * @date    Jul 30, 2025
+ * @author  eden
  */
+
 #include "System.h"
 
-volatile uint8_t user_btn_flag = 0x00U;
-volatile uint8_t timer_logger_flag = 0x00U;
+/* ------------------------------------------------------------------------- */
+/*                               Global Variables                            */
+/* ------------------------------------------------------------------------- */
+volatile uint8_t user_btn_flag       = 0x00U;
+volatile uint8_t timer_logger_flag   = 0x00U;
 volatile system_states_t system_state = SYS_STATE_IDLE;
 
 uint16_t adc_buf = 0x00U;
 
 module_config_t config = {
-    .start_msg = {'s','t','a','r','t'},
-    .stop_msg = {'s','t','o','p'}
+    .start_msg = { 's', 't', 'a', 'r', 't' },
+    .stop_msg  = { 's', 't', 'o', 'p' }
 };
 
+/* ------------------------------------------------------------------------- */
+/*                             Function Definitions                          */
+/* ------------------------------------------------------------------------- */
+
+/**
+ * @brief Main application loop
+ */
 void main_system_loop(void) {
-	if (user_btn_flag) {
-		HAL_Delay(DEBOUNCE_TIME);
-		HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_5);
-		if (system_state == SYS_STATE_RUNNING) {
-			adc_stop_mesure();
-			system_state = SYS_STATE_IDLE;
-		} else {
-			adc_start_mesure();
-			system_state = SYS_STATE_RUNNING;
-		}
+    if (user_btn_flag) {
+        HAL_Delay(DEBOUNCE_TIME);
+        HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_5);
 
-		user_btn_flag = 0x00U;
-	}
+        if (system_state == SYS_STATE_RUNNING) {
+            adc_stop_mesure();
+            system_state = SYS_STATE_IDLE;
+        } else {
+            adc_start_mesure();
+            system_state = SYS_STATE_RUNNING;
+        }
 
+        user_btn_flag = 0x00U;
+    }
 
-	switch (system_state) {
-	case SYS_STATE_IDLE:
+    switch (system_state) {
+        case SYS_STATE_IDLE:
+            break;
 
-		break;
-	case SYS_STATE_RUNNING:
-		if (timer_logger_flag) {
-			printf("adc_value : %u \r\n", adc_buf);
-			timer_logger_flag = 0x00U;
-		}
+        case SYS_STATE_RUNNING:
+            if (timer_logger_flag) {
+                printf("adc_value : %u \r\n", adc_buf);
+                timer_logger_flag = 0x00U;
+            }
+            break;
 
-		break;
-	case SYS_STATE_ERROR:
-		printf("Error\r\n");
-		system_state = SYS_STATE_IDLE;
-		break;
+        case SYS_STATE_ERROR:
+            printf("Error\r\n");
+            system_state = SYS_STATE_IDLE;
+            break;
 
-	default:
-		printf("Unexpected system state\r\n");
-		system_state = SYS_STATE_IDLE;
-		break;
-	}
-
+        default:
+            printf("Unexpected system state\r\n");
+            system_state = SYS_STATE_IDLE;
+            break;
+    }
 }
 
+/**
+ * @brief Start ADC measurement
+ */
 system_module_error_t adc_start_mesure(void) {
-	if (HAL_TIM_Base_Start_IT(&htim10) != HAL_OK){
-		return SYSTEM_ERROR_TIM10;
-	}
-	if (HAL_TIM_Base_Start_IT(&htim11) != HAL_OK){
-		return SYSTEM_ERROR_TIM11;
-	}
-	if (HAL_ADC_Start_DMA(&hadc1, (uint32_t*)&adc_buf, ADC_SIZE) != HAL_OK){
-		return SYSTEM_ERROR_ADC;
-	}
-	return SYSTEM_OK;
+    if (HAL_TIM_Base_Start_IT(&htim10) != HAL_OK) {
+        return SYSTEM_ERROR_TIM10;
+    }
+
+    if (HAL_TIM_Base_Start_IT(&htim11) != HAL_OK) {
+        return SYSTEM_ERROR_TIM11;
+    }
+
+    if (HAL_ADC_Start_DMA(&hadc1, (uint32_t *)&adc_buf, ADC_SIZE) != HAL_OK) {
+        return SYSTEM_ERROR_ADC;
+    }
+
+    return SYSTEM_OK;
 }
 
+/**
+ * @brief Stop ADC measurement
+ */
 system_module_error_t adc_stop_mesure(void) {
-	if (HAL_TIM_Base_Stop_IT(&htim10) != HAL_OK){
-		return SYSTEM_ERROR_TIM10;
-	}
-	if (HAL_TIM_Base_Stop_IT(&htim11) != HAL_OK){
-		return SYSTEM_ERROR_TIM11;
-	}
-	if (HAL_ADC_Stop_DMA(&hadc1) != HAL_OK){
-		return SYSTEM_ERROR_ADC;
-	}
-	return SYSTEM_OK;
+    if (HAL_TIM_Base_Stop_IT(&htim10) != HAL_OK) {
+        return SYSTEM_ERROR_TIM10;
+    }
+
+    if (HAL_TIM_Base_Stop_IT(&htim11) != HAL_OK) {
+        return SYSTEM_ERROR_TIM11;
+    }
+
+    if (HAL_ADC_Stop_DMA(&hadc1) != HAL_OK) {
+        return SYSTEM_ERROR_ADC;
+    }
+
+    return SYSTEM_OK;
 }
 
-
+/**
+ * @brief TIM Period Elapsed Callback
+ */
 void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim) {
-	if (htim == &htim10) {
-		timer_logger_flag = 0x01U;
-	}
-	if (htim == &htim11) {
-		HAL_ADC_Start_DMA(&hadc1, (uint32_t*)&adc_buf, ADC_SIZE);
-	}
+    if (htim == &htim10) {
+        timer_logger_flag = 0x01U;
+    }
+
+    if (htim == &htim11) {
+        HAL_ADC_Start_DMA(&hadc1, (uint32_t *)&adc_buf, ADC_SIZE);
+    }
 }
 
+/**
+ * @brief GPIO External Interrupt Callback
+ */
 void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin) {
-	user_btn_flag = 0x01U;
+    (void)GPIO_Pin;
+    user_btn_flag = 0x01U;
 }
 
-
+/**
+ * @brief UART RX Event Callback (ReceiveToIdle)
+ */
 void HAL_UARTEx_RxEventCallback(UART_HandleTypeDef *huart, uint16_t Size) {
+    (void)Size;
 
-	if (huart == &huart2) {
-		if (strncmp((const char *)rx_buffer, (const char *)config.start_msg, START_MSG_SIZE) == 0) {
-			if (adc_start_mesure() != SYSTEM_OK) {
-				system_state = SYS_STATE_ERROR;
-			} else {
-				system_state = SYS_STATE_RUNNING;
-			}
+    if (huart == &huart2) {
+        if (strncmp((const char *)rx_buffer, (const char *)config.start_msg, START_MSG_SIZE) == 0) {
+            if (adc_start_mesure() != SYSTEM_OK) {
+                system_state = SYS_STATE_ERROR;
+            } else {
+                system_state = SYS_STATE_RUNNING;
+            }
+        } else if (strncmp((const char *)rx_buffer, (const char *)config.stop_msg, STOP_MSG_SIZE) == 0) {
+            if (adc_stop_mesure() != SYSTEM_OK) {
+                system_state = SYS_STATE_ERROR;
+            } else {
+                system_state = SYS_STATE_IDLE;
+            }
+        } else {
+            system_state = SYS_STATE_ERROR;
+        }
 
-		} else if (strncmp((const char *)rx_buffer, (const char *)config.stop_msg, STOP_MSG_SIZE) == 0) {
-			if (adc_stop_mesure() != SYSTEM_OK) {
-				system_state = SYS_STATE_ERROR;
-			} else {
-				system_state = SYS_STATE_IDLE;
-			}
-		} else {
-			system_state = SYS_STATE_ERROR;
-
-		}
-
-		HAL_UARTEx_ReceiveToIdle_DMA(huart, rx_buffer, RX_BUFF_SIZE);
-		/* add memset ?*/
-	}
-
+        HAL_UARTEx_ReceiveToIdle_DMA(huart, rx_buffer, RX_BUFF_SIZE);
+        /* Consider adding: memset(rx_buffer, 0, RX_BUFF_SIZE); */
+    }
 }
-
-
-
